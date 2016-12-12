b9e978b89a8fd48a3166555357c3ff4181af7e9a
MartinFernandoAndres <martinfernandoandres1996@gmail.com>
2016-12-12 11:48:25 -0300

    Limpio proyecto

--- src/main/java/controller/SharedController.java
+++ src/main/java/controller/SharedController.java
@@ -1,12 +1,8 @@
 package controller;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import clases.Book;
 
 @Controller
 public class SharedController {
